c3cec7e77db5d6c32023275c43219dd8ef130143
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-14 23:45:41 +0800

    Commit 1

--- Binary Tree Exercise 1.py
+++ Binary Tree Exercise 1.py
@@ -5,3 +5,24 @@ print("********** BSCOE 2-2 **********")
 
 #Adding the methods to the BinarySearchTreeNode class created in main video tutorial
 #Using my fullname as the content of the binary tree
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return # node already exist
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
