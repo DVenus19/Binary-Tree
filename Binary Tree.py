c34714a5656d514b91730f2eb869a2d22da4d9c2
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-14 23:31:56 +0800

    Commit 41

--- Binary Tree.py
+++ Binary Tree.py
@@ -3,65 +3,58 @@ print("**********Programmed by : Donasco,Venus M.")
 print("********** BSCOE 2-2 **********")
 
 class BinarySearchTreeNode:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-    def add_child(self,data):
+    def add_child(self, data):
         if data == self.data:
-            return
+            return # node already exist
 
         if data < self.data:
-            #adding data in the left subtree
             if self.left:
                 self.left.add_child(data)
             else:
                 self.left = BinarySearchTreeNode(data)
         else:
-            #adding data in the right subtree
             if self.right:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
 
-    def in_order_travesal(self):
-         elements = []
 
-         #visit left tree
-         if self.left:
-           elements += self.left.in_order_traversal()
-
-         #visit base node
-         elements.append(self.data)
-
-         #visit right tree
-         if self.right:
-           elements += self.right.in_order_traversal()
-
-         return elements
-
-    def search(self,val):
+    def search(self, val):
         if self.data == val:
             return True
 
         if val < self.data:
             if self.left:
-                self.left.search(val)
+                return self.left.search(val)
             else:
                 return False
 
         if val > self.data:
-            if val < self.data:
-                if self.right:
-                    self.right.search(val)
-                else:
-                    return False
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
 
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
 
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
 
 
 def build_tree(elements):
+    print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
 
     for i in range(1,len(elements)):
@@ -69,9 +62,6 @@ def build_tree(elements):
 
     return root
 
-if __name__ == '__main__':
-    numbers = [17,4,1,20,9,23,18,34,18,4]
-    numbers_tree = build_tree(numbers)
-    print(numbers_tree.in_order_travesal())
+
 
 
