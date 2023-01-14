f3897e7b9ea7ac87a7a7f7007dfae53dfd0cd189
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-14 23:44:57 +0800

    Initial Commit

--- Binary Tree Exercise 1.py
+++ Binary Tree Exercise 1.py
@@ -3,3 +3,5 @@ print("**********Programmed by : Donasco,Venus M.")
 print("********** BSCOE 2-2 **********")
 
 
+#Adding the methods to the BinarySearchTreeNode class created in main video tutorial
+#Using my fullname as the content of the binary tree
