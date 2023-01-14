3bae9e561ed7b56432c9c3e7d21f82799ee4f05c
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-15 01:46:50 +0800

    Initial Commit

--- Binary Tree Exercise 2.py
+++ Binary Tree Exercise 2.py
@@ -1,3 +1,5 @@
 print("**********DSA - Binary Tree Exercise 2**********")
 print("**********Programmed by : Donasco,Venus M.")
 print("********** BSCOE 2-2 **********")
+
+#Modify delete method in class BinarySearchTreeNode class to use min element from left subtree and using max value from left subtree
